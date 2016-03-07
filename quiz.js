2200591aa5367eecadc9459539e54f43735a7b81
Odie Joseph <Odie Joseph>
2016-03-07 17:26:28 -0600

    added semicolons, clear function should be on line 49

--- quiz.js
+++ quiz.js
@@ -9,26 +9,25 @@
 
 // create function that gets the values that the user places inside spaces and returns it
 function getValue(){
-	var treeHeight = document.getElementById("heightInput").value
-	var treeChar = document.getElementById("charInput").value
+	var treeHeight = document.getElementById("heightInput").value;
+	var treeChar = document.getElementById("charInput").value;
 
 	var input = {
 	height: treeHeight,
 	character: treeChar
 	};
-	console.log(treeHeight);
-	return input
+	return input;
 }
 
 // create function that checks values (using a function) that are inputted by user and shoots out alert if they do not put any values and  
 function checkInput(){
-	var object = getValue()	
+	var object = getValue();
 	console.log("object value", object);
 	if (object.height === "" || object.character === ""){
-		alert("Please input Stuff")
+		alert("Please input Stuff");
 	}	
 	else { 
-		treetop(object)
+		treetop(object);
 	};
 	return false;
 }
@@ -40,8 +39,8 @@ function checkInput(){
 // create function that builds tree using height and character input values and makes space between characters
 function treetop(object) {
 	for (var i = 1; i <= object.height ; i++){
-		var spaceJam = object.height -i
-		var demChars = (i * 2) - 1
+		var spaceJam = object.height -i;
+		var demChars = (i * 2) - 1;
 		console.log( " ".repeat(spaceJam)+ object.character.repeat(demChars));
 	};
 }
@@ -55,14 +54,15 @@ function clear() {
 
 
 // Add event listener for click that clears values entered by user.
-document.getElementById("butt2").addEventListener("click", clear)
+document.getElementById("butt2").addEventListener("click", clear);
 // Add event listener for click that checks what is inputed into lines and then starts the tree
-document.getElementById("butt").addEventListener("click", checkInput)
+document.getElementById("butt").addEventListener("click", checkInput);
 
 document.getElementById("charInput").addEventListener("keypress", function (e) {
     var key = e.which || e.keyCode;
-    if (key === 13) 
-    	checkInput()
+    if (key === 13) {
+    	checkInput();
+    }
     });
 
 
